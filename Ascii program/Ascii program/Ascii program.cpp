f5362c368f4e5878e5581b9ad0b22d374d51b8d1
IthinkIknowhowtocode <97357235+IthinkIknowhowtocode@users.noreply.github.com>
2022-10-20 17:43:11 -0700

    Add files via upload

---
+++ Ascii program/Ascii program/Ascii program.cpp
@@ -0,0 +1,13 @@
+// Ascii program.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << int("a");
+    cout << int("A");
+    cout << char(97);
+
+
+}
